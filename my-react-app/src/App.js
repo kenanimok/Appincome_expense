ec64fd328aec78c91a5f58ae44d0f564c4718551
kenanimok <89970690+kenanimok@users.noreply.github.com>
2022-02-08 21:37:53 +0700

    create_useState

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Transaction from './Transaction';
+import Formcomponent from './Formcomponent';
 
 
 function App() {
+   const desingn = {color:"red",textAlign:"center",fontSize:'1.5rem'}
   return (
-     <div>
-        <h1 style={{coloe:"red",textAlign:"center"}}>โปรแกรมรายรับ รายจ่าย</h1>
+      <div className="container">
+         <h1 style={{coloe:"red",textAlign:"center"}}>โปรแกรมรายรับ รายจ่าย</h1>
+           
+            
+            <Formcomponent></Formcomponent>
             <Transaction></Transaction>
-     </div>
+
+      </div>
+
+    
 
   );
 }
---
+++ my-react-app/src/Formcomponent.js
@@ -0,0 +1,46 @@
+import {useState} from 'react';
+const Formcomponent =()=>{
+
+    const [title,setTitle]=  useState('')
+    const [amount,setAmount]= useState(0)
+
+    const inpuTitle = (event)=>{
+    //  console.log(event.target.value)
+     setTitle(event.target.value)
+    }
+
+    const inputAmount = (event)=>{
+        // console.log(event.target.value)
+        setAmount(event.target.value)
+       }
+
+
+    const saveItem =(event)=>{
+        event.preventDefault()
+     const itemData ={
+         title:title,
+        amount:Number(amount)
+     }
+     console.log("ดุitemdata",itemData);
+     setTitle('')
+     setAmount(0)
+    }
+        return (
+            <div>
+                 <form onSubmit ={saveItem}>
+                      <div className="form-control">
+                          <label> ชื่อรายการ </label>
+                          <input type="text" onChange={inpuTitle} value={title}/>
+                      </div>
+                      <div className="form-control">
+                          <label> จำนวนเงิน </label>
+                          <input type="number" onChange={inputAmount}  value={amount}/>
+                      </div>
+
+                        <div> <button type="submit">เพิ่มข้อมูล</button></div>
+
+                 </form>
+            </div>            
+        );
+}
+export default Formcomponent
